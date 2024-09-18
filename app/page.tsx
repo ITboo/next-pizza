4eb845831d6b9167593151136b6360bb9554da7f
ITboo <annaacdc@tut.by>
2024-09-18 19:08:12 +0300

    feat: add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Container } from "@/components/shared/container";
+import { Header } from "@/components/shared/header";
+import { Title } from "@/components/shared/title";
 
 export default function Home() {
   return (
-    <>
-    <h1>Home</h1>
-    <Button variant="outline">Войти</Button>
-    </>
+    <main className="min-h-screen bg-white rounded-3xl">
+      <Header />
+      <Container className="mt-5">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>    
+    </main>
   );
 }
